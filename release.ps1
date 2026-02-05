2f6164482567a81c3ceb5c87abdf58e1e922e6a3
Filip Merlung <filip.merlung@gmail.com>
2026-02-05 17:07:08 +0100

    initial commit using generated template

---
+++ release.ps1
@@ -0,0 +1,2 @@
+dotnet publish Flow.Launcher.Plugin.Pomodoro -c Release -r win-x64 --no-self-contained
+Compress-Archive -LiteralPath Flow.Launcher.Plugin.Pomodoro/bin/Release/win-x64/publish -DestinationPath Flow.Launcher.Plugin.Pomodoro/bin/Pomodoro.zip -Force
