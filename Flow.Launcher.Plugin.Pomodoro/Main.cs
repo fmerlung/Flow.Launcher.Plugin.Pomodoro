524b2feac45bf7c9fd2e3dc18ea86203467b250e
Filip Merlung <filip.merlung@gmail.com>
2026-02-08 09:05:24 +0100

    wip toast notification

--- Flow.Launcher.Plugin.Pomodoro/Main.cs
+++ Flow.Launcher.Plugin.Pomodoro/Main.cs
@@ -1,12 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.XPath;
 using Flow.Launcher.Plugin;
+using Microsoft.Toolkit.Uwp.Notifications;
+
+#pragma warning disable 1591
 
 namespace Flow.Launcher.Plugin.Pomodoro
 {
     public class Pomodoro : IPlugin
     {
         private PluginInitContext _context;
+        private int totalQueries = 0;
+        private Timer timer = new Timer(TimerCallback);
 
         public void Init(PluginInitContext context)
         {
@@ -15,7 +24,39 @@ namespace Flow.Launcher.Plugin.Pomodoro
 
         public List<Result> Query(Query query)
         {
-            return new List<Result>();
+            totalQueries++;
+
+
+            Result result = new Result();
+            result.Title = $"Query count: {totalQueries}";
+
+            if (query.Search == "start")
+            {
+                result.Action = e =>
+                {
+                    StartTimer();
+                    return true;
+                };
+            }
+
+            List<Result> output = new List<Result>();
+            output.Add(result);
+            return output;
         }
+
+        private void StartTimer()
+        {
+            // Start the timer to execute every 25 minutes (1500 seconds)
+            timer.Change(0, 5);
+        }
+        private static void TimerCallback(object state)
+        {
+            new ToastContentBuilder()
+                .AddText("Pomodoro Timer")
+                .Show();
+
+            Console.WriteLine("Timer callback executed at: " + DateTime.Now);
+        }
+
     }
 }
